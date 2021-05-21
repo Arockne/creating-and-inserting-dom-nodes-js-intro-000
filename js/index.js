043804abc6053fc3af6b0f6134952d34a6030cbf
Daniel Calvin <arockne2003@yahoo.com>
2021-05-21 21:56:59 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -7,10 +7,10 @@ document.body.appendChild(element);
 element.style.textAlign = 'center';
 
 var ul = document.createElement('ul');
+element.appendChild(ul);
 
 for (let i = 0; i < 3; i++) {
   let li = document.createElement('li');
   li.innerHTML = (i+1).toString();
   ul.appendChild('ul');
 }
-element.appendChild(ul);
